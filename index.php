b335379ab3a5df824e0af0c1f48f12bf002e552a
tom <tbeikman@fas.harvard.edu>
2014-09-28 21:22:02 -0400

    W3 Validation updates

--- index.php
+++ index.php
@@ -45,6 +45,12 @@
         
         <!-- Ideally the password is printed here, but an error message is possible too -->
         <h2><?php echo getPassword(); ?></h2>
-
+        
+        <!-- The site validates 4.01 strict with w3.org 
+         <p>
+            <a href="http://validator.w3.org/check?uri=referer"><img
+            src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Strict" height="31" width="88"></a>
+        </p>
+        -->
     </body>
 </html>
